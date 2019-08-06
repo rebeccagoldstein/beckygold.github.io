1f480f84a9e5fbba9d31dc35e04299301c9b2f81
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-08-06 16:09:14 -0700

    Basic layout with buttons

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// When user clicks Basic Websites button
+$(document).on("click", "#basic-sites-button", function() {
+    // Display #basic-sites-blurb
+    $("#basic-sites-blurb").removeClass("hide");
+});
+
+// When user clicks on Full Stack button
+$(document).on("click", "#full-stack-button", function() {
+    // Display #full-stack-blurb
+    $("#full-stack-blurb").removeClass("hide");
+});
