8f6022d4e6af6aea381927f0376ec689ce73463b
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-08-07 17:22:27 -0700

    Added logic for freelance page navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,38 @@
-// When user clicks Basic Websites button
-$(document).on("click", "#basic-sites-button", function() {
-    // Display #basic-sites-blurb
-    $("#basic-sites-blurb").removeClass("hide");
+// When user clicks #basic-sites-button
+$(document).on("click", "#basic-sites-button", function () {
+    if (
+        $("#basic-sites-blurb").hasClass("hide")
+    ) {
+        $("#basic-sites-blurb").removeClass("hide");
+        $("#freelance-main").addClass("hide");
+    };
 });
 
 // When user clicks on Full Stack button
-$(document).on("click", "#full-stack-button", function() {
-    // Display #full-stack-blurb
-    $("#full-stack-blurb").removeClass("hide");
+$(document).on("click", "#full-stack-button", function () {
+    if (
+        $("#full-stack-blurb").hasClass("hide")
+    ) {
+        $("#full-stack-blurb").removeClass("hide");
+        $("#freelance-main").addClass("hide");
+    };
 });
+
+// When a user clicks on the Freelance Back Button
+$(document).on("click", "#freelance-back-button", function () {
+    // If Freelance Main and Full Stack Blurb are hidden, hide Basic Sites Blurb and display Main Blurb
+    if (
+        $("#freelance-main").hasClass("hide") && $("#full-stack-blurb").hasClass("hide")
+    ) {
+        $("#freelance-main").removeClass("hide");
+        $("#basic-sites-blurb").addClass("hide");
+    };
+
+    // If Freelance Main and Basic Sites Blurb are hidden, hide Full Stack Blurb and display Main Blurb
+    if (
+        $("#freelance-main").hasClass("hide") && $("#basic-sites-blurb").hasClass("hide")
+    ) {
+        $("#freelance-main").removeClass("hide");
+        $("#full-stack-blurb").addClass("hide");
+    };
+})
